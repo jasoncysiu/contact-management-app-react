86addfac07f801c5985dd7dde0d8f8e2a639adb2
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-02-11 22:16:32 +1100

    After the tute

---
+++ src/components/ContactList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+
+    const deleteConactHandler = (id) => {
+        props.getContactId(id);
+      };
+      
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+           <ContactCard contact = {contact}
+           clickHander={deleteConactHandler}
+           key={contact.id}
+           ></ContactCard>
+        );
+      });
+
+  return <div className="ui celled list">
+    {renderContactList}
+  </div>;
+};
+
+export default ContactList;
